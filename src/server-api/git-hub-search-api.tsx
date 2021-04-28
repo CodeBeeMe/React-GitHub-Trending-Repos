8237c7cedf1bcec2a52f93322d6e4b4a67a5a4dd
Cătălin Anghel-Ursu <cat.ang.dev@gmail.com>
2021-04-28 11:27:30 +0300

    Update git-hub-search-api.tsx
    Commenting out the authorization line to make a token-less call, since my personal token is just valid for my use.

--- src/server-api/git-hub-search-api.tsx
+++ src/server-api/git-hub-search-api.tsx
@@ -8,8 +8,13 @@ export class GitHubSearchAPI {
     public static async getLastWeekTrendingRepos(): Promise<GitHubRepositoryDTO> {
         const { data } = await axios.get(this.gitHubTrendingReposUrl, {
             method: 'GET',
+            //Due to Githubs own API call rate limitation to 10 calls/hour an authorization token can be used to increase the call limit.
+            //https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token
             headers: {
-                authorization: 'token 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',
+                //Please add you own personal generated token below, replacing my old one and un-comment the authorization line if you want to make a token based call
+                //if not, a normal call without token will be made with the 10 calls/hour limit.
+                
+                //authorization: 'token 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',
             },
         });
         return data;
